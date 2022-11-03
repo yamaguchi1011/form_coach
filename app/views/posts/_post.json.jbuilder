5b4f55a8023087ff4750eb8a5db6ae6dcc2c57a7
yamaguchi <yamaguchi1001@icloud.com>
2022-11-03 19:14:16 +0900

    scaffoldでUserとPostsを作成した

---
+++ app/views/posts/_post.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! post, :id, :video, :body, :angle, :image, :pitching_form, :created_at, :updated_at
+json.url post_url(post, format: :json)
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,10 @@ module FormCoach
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
