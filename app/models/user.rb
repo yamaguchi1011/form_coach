d326759b89a44989b4d93678cc30ed47baac8c64
yamaguchi <yamaguchi1001@icloud.com>
2022-11-07 07:27:47 +0900

    sorceryでログイン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true
+  validates :email, presence: true
   validates :name, presence: true, length: { maximum: 255}
   
 end
