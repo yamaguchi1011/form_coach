8caccb6b7f0871f5ea107e85b1540f4a885ec7d0
yamaguchi <yamaguchi1001@icloud.com>
2023-02-04 11:26:11 +0900

    コメント投稿機能追加、詳細ページ作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   # attr_accessor :video_cache
   mount_uploader :video, VideoUploader
   belongs_to :user
+  has_many :comments, dependent: :destroy 
 
   validates :body, presence: true, length: { maximum: 280}
   validates :video, presence: true
