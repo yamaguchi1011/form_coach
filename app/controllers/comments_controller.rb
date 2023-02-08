80ba59b87feeee2e8bcd7f25655046f244cc0df8
yamaguchi <yamaguchi1001@icloud.com>
2023-02-08 22:56:10 +0900

    コメント削除機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,11 +10,19 @@ class CommentsController < ApplicationController
     else
       redirect_to board_path(comment.post), danger: t('defaults.message.created', item: Comment.model_name.human)
     end
+
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
+    redirect_to post_path(@comment.post), success: t('defaults.message.deleted', item: Comment.model_name.human)
   end
 
   def effectives
     @effective_comments = current_user.effective_comments.includes(:user).order(created_at: :desc)
   end
+
 
   private
 
