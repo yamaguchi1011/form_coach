2a1706a5710ea852f0918261d22e3decc04cb603
yamaguchi <yamaguchi1001@icloud.com>
2023-02-08 22:56:10 +0900

    コメント削除機能追加

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,5 @@
 import { Application } from "@hotwired/stimulus"
-import Rails from '@rails/ujs';
 
-Rails.start();
 const application = Application.start()
 
 // Configure Stimulus development experience
