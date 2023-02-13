f1052d71f5e4c1cbe66961bd548432be6ee71efe
yamaguchi <yamaguchi1001@icloud.com>
2023-02-14 07:25:37 +0900

    pythoで画像を分析して画像自体に結果を描画した。プロフィール画面を追加した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @posts = User.posts.order("created_at DESC")
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
