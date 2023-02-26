0f63944fc6e95d09867a042ac093cd7a6b0cf5a8
yamaguchi <yamaguchi1001@icloud.com>
2023-02-26 09:04:04 +0900

    ログイン失敗時にフラッシュメッセージを追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
+// ターボを無効にするコード
+// import { Turbo } from "@hotwired/turbo-rails"
+// Turbo.session.drive = false
