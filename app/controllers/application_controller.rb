07e666b9360e76b38555513cc762504cb40576b7
yamaguchi <yamaguchi1001@icloud.com>
2023-03-12 21:43:34 +0900

    viewを修正し、turboでの処理を追加した（無限スクロール他）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+  before_action :set_search
   add_flash_types :success, :info, :warning, :danger
   def appropriate_flash
     case
@@ -9,4 +10,12 @@ class ApplicationController < ActionController::Base
       flash
     end
   end
+
+  def set_search
+    @q = Post.ransack(params[:q])
+    @posts = @q.result(distinct: true).includes(:user).order(created_at: :desc)
+    # @posts = Post.all.includes([:user, :comments]).order(created_at: :desc)
+    # @comments = Comment.includes(:post)
+  end
+
 end
