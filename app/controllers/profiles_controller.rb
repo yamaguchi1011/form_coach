db85877b4b4ba699a9d4c48573769232fc6de729
yamaguchi <yamaguchi1001@icloud.com>
2023-03-18 14:23:53 +0900

    投稿新規作成画面を作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,10 +8,10 @@ class ProfilesController < ApplicationController
   def update
     if @user.update(user_params)
       # redirect_to profile_path, success: t('defaults.message.updated', item: User.model_name.human)
-      flash.now['success']= t('defaults.message.updated', item: User.model_name.human)
+      flash.now.notice= t('defaults.message.updated', item: User.model_name.human)
     else
-      flash.now['danger'] = t('defaults.message.not_updated', item: User.model_name.human)
-      render :edit
+      # flash.now['danger'] = t('defaults.message.not_updated', item: User.model_name.human)
+      render :edit, status: :unprocessable_entity
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path, success: t('.success')
+      redirect_to login_path, notice: t('.success')
     else
+      flash.now.alert = t('.fail')
       render :new, status: :unprocessable_entity
     end
   end
