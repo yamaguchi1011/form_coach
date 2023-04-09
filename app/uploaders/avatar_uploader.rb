22fd6e519546ab902741be02b52174ac78664fad
yamaguchi <yamaguchi1001@icloud.com>
2023-04-09 16:56:59 +0900

    プロフィール画像読み込み修正

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,7 +15,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    'sample.jpg'
+    'sample.png'
   end
 
   # Process files as they are uploaded:
